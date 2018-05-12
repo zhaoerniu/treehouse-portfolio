cd474c3efbdde45350f1d235b660b3e7e9b5d9ca
zhaoerniu <zhaoyinmusic@gmail.com>
2018-05-12 20:30:24 +0800

    Porting existing headers and footers into WordPress

--- footer.php
+++ footer.php
@@ -1,1 +1,10 @@
-<p>Footer</p>
+<div class="footer-clear"></div>
+<footer class="row no-max pad">
+           
+  <p>Copyright <?php echo date('Y'); ?></p>
+</footer>
+
+  <?php wp_footer(); ?>
+
+  </body>
+</html>
